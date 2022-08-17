67d1a61eb33f5b812fc6cbc6c83c12709d63b5aa
strelok <strelok@ecwid.com>
2022-08-17 18:03:48 +0400

    newPageSwitch

--- index.js
+++ index.js
@@ -2,5 +2,13 @@ Ecwid.OnAPILoaded.add(function() {
     console.log("Ecwid storefront JS API has loaded");
 });
 Ecwid.OnPageSwitch.add(function(page) {
-    console.log(page.type)
+    if (page.type === "PRODUCT") {
+        console.log(page.type)
+        window.location.href = "index.html?type=product&id=" + page.productId
+        return false
+    } else if (page.type === "CATEGORY") {
+        console.log(page.type)
+        window.location.href = "index.html?type=category&id=" + page.categoryId
+        return false
+    }
 })
