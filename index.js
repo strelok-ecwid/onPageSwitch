4988e6e415fa2ef76ba57e091ac5218e71219eda
strelok <strelok@ecwid.com>
2022-08-17 19:23:25 +0400

    name

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ Ecwid.OnAPILoaded.add(function() {
 });
 Ecwid.OnPageSwitch.add(function(page) {
     if (page.type === "PRODUCT") {
-        console.log(page.type)
-        window.location.href = "index.html?type=product&id=" + page.productId
-        return false
+        
+       console.log(page.name)
     } else if (page.type === "CATEGORY") {
-        console.log(page.type)
-        window.location.href = "index.html?type=category&id=" + page.categoryId
-        return false
+        
+        console.log(page.name)
     }
 })
